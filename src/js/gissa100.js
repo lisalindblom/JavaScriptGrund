0b78c82962e3a9ed6926cac01a7094bc9b14481f
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2022-10-18 13:55:47 +0200

    .

--- src/js/gissa100.js
+++ src/js/gissa100.js
@@ -2,6 +2,9 @@ let number = document.getElementById("number");
 let userSubmit = document.getElementById("userSubmit");
 let output = document.getElementById("output");
 
+//Kopplar scss
+userSubmit.className = "button";
+
 userSubmit.addEventListener("click", submit);
 
 function submit() {
