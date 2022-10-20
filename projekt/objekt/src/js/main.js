ef90124fe970134783311fb3be66fc88267b5fca
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2022-10-20 15:02:41 +0200

    loppar listan

--- projekt/objekt/src/js/main.js
+++ projekt/objekt/src/js/main.js
@@ -1,13 +1,24 @@
 class Person {
   firstname;
+  lastname;
   age;
-  constructor(firstname, age) {
+  length;
+  constructor(firstname, lastname, age, length) {
     this.firstname = firstname;
+    this.lastname = lastname;
     this.age = age;
+    this.length = length;
   }
 }
 
 window.onload = function () {
-  let me = new Person("Lisa", "26");
-  console.log(me);
+  let me = new Person("Lisa", "Lindblom", 26, 172);
+  let mira = new Person("Mira", "Vitsas", 25, 174);
+  let personList = [me, mira];
+  let person = document.getElementById("person");
+  for (i = 0; i < personList.length; i++) {
+    person.innerHTML += personList[i].firstname + " ";
+    person.innerHTML += personList[i].lastname + " ";
+    console.log(me);
+  }
 };
