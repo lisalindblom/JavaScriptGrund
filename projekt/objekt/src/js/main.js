1b71c0daa53ab94e0b31c65c598eb679ff925700
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2022-10-20 15:09:41 +0200

    lade till kommentarer

--- projekt/objekt/src/js/main.js
+++ projekt/objekt/src/js/main.js
@@ -1,3 +1,4 @@
+//skapar klassen Person
 class Person {
   firstname;
   lastname;
@@ -11,11 +12,14 @@ class Person {
   }
 }
 
+// skapar en funktion som körs när sidan laddar
 window.onload = function () {
   let me = new Person("Lisa", "Lindblom", 26, 172);
   let mira = new Person("Mira", "Vitsas", 25, 174);
+  // gör personerna till en lista för att kunna loopa igenom och skriva ut värdet
   let personList = [me, mira];
   let person = document.getElementById("person");
+  //loopen går lika många varv som listan är lång
   for (i = 0; i < personList.length; i++) {
     person.innerHTML += personList[i].firstname + " ";
     person.innerHTML += personList[i].lastname + " ";
